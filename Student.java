25cf2a1ca27b884ed028fb62d0c06589459ef2f6
setomulyovic <32457267+setomulyovic@users.noreply.github.com>
2017-11-07 11:15:11 +0700

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,26 @@
+public class Student extends Person{
+    private long id;
+    private int rank;
+
+    public Student(String name,long id) {
+        super(name);
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+    
+}
